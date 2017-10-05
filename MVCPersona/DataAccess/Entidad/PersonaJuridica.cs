13e47b78ff02dab2a5833fec8d7f366d6b93c9f3
david226 <30418773+david226@users.noreply.github.com>
2017-10-05 17:50:45 -0500

    AgregueEntidad

---
+++ MVCPersona/DataAccess/Entidad/PersonaJuridica.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.clases
+{
+    public class PersonaJuridica
+    {
+        public string ruc { get; set; }
+        public string urlPaginaWeb { get; set; }
+    }
+}
