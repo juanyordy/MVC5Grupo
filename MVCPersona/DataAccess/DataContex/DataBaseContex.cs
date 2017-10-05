ea82e41aec08d1adec9c831d32ac83295113be6c
juanyordyguillen <juanyordyguillen@gmail.com>
2017-10-05 18:41:45 -0500

    suviendo la conexion

---
+++ MVCPersona/DataAccess/DataContex/DataBaseContex.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess.clases;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DataContex
+{
+   public class DataBaseContex: DbContext
+    {
+        public DataBaseContex() : base("MyConexion")
+        {
+
+        }
+        public DbSet<PersonaJuridica> PersonaJuridica{get;set;}
+        public DbSet<PersonaNatural> PersonaNatural { get; set; }
+    }
+}
